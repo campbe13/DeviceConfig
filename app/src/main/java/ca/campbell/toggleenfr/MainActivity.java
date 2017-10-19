f13756f7960ba878fe43bf309021549107421d9e
P Campbell <pcampbell.edu@gmail.com>
2017-10-18 22:24:07 -0400

    change from toggle to set launch language settings activity from button, easily change language launch app settings from button, easily stop or delete data for ap changing to general purpose for testing student code

--- app/src/main/java/ca/campbell/toggleenfr/MainActivity.java
+++ app/src/main/java/ca/campbell/toggleenfr/MainActivity.java
@@ -31,21 +31,47 @@ public class MainActivity extends AppCompatActivity {
         conf = getApplicationContext().getResources().getConfiguration();
         current = (TextView)findViewById(R.id.current);
         setCurrent();
+        setBuildStuff();
         Log.d(TAG,"Current SDK " + Build.VERSION.SDK_INT);
     }
-    protected void setCurrent() {
+     protected void setBuildStuff() {
+         String msg;
+         msg = getResources().getString(R.string.apivers);
+         msg += " "+Build.VERSION.SDK_INT;
+         Log.d(TAG, msg);
+         ((TextView)findViewById(R.id.apivers)).setText(msg);
+
+         msg = getResources().getString(R.string.brand);
+         msg += " "+Build.BRAND;
+         Log.d(TAG, msg);
+         ((TextView)findViewById(R.id.brand)).setText(msg);
+
+         msg = getResources().getString(R.string.device);
+         msg += " "+Build.DEVICE;
+         msg +=  " hardware "+Build.HARDWARE;
+         msg +=  " manufacturer "+Build.MANUFACTURER;
+         msg +=  " model "+Build.MODEL;
+         Log.d(TAG, msg);
+         ((TextView)findViewById(R.id.device)).setText(msg);
+
+     }
+     protected void setCurrent() {
         String msg;
-        msg = getResources().getString(R.string.current);
+        msg = res.getString(R.string.current);
         if (Build.VERSION.SDK_INT >= 24) {
             // getLocales() / setLocales() only api level 24 & up
             msg += " " + conf.getLocales().get(0);
         } else {
 
-            msg += " " + res.getString(R.string.current) + " " + conf.locale;
+            msg += " " + conf.locale;
         }
         Log.d(TAG, msg);
         current.setText(msg);
+
     }
+
+    // was going to do this, decided the other way is easier (see other)
+    /*
     public void setEnglish(View v) {
         setTheLocale(EN_CA);
     }
@@ -65,10 +91,14 @@ public class MainActivity extends AppCompatActivity {
     public void setFrancais(View v) {
         setTheLocale(FR_CA);
     }
+ */
     public void setOtherLanguage(View v) {
         startActivity(new Intent(Settings.ACTION_LOCALE_SETTINGS));
     }
-
+    public void appSettings(View v) {
+        startActivity(
+                new Intent(android.provider.Settings.ACTION_APPLICATION_SETTINGS));
+    }
     public void onConfigurationChanged(Configuration config)
     {
         Log.d(TAG, "onConfigurationChanged()");
